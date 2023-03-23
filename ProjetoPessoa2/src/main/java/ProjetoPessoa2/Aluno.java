1128c113fda59b413d5afd23fcd02a28b1bec5cb
Geovani dos Passos <84425834+GeovaniPassos@users.noreply.github.com>
2023-03-23 14:23:52 -0300

    Aula 11 - Herança
    Criação do exemplo em java

---
+++ ProjetoPessoa2/src/main/java/ProjetoPessoa2/Aluno.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProjetoPessoa2;
+
+/**
+ *
+ * @author Geovani-PC
+ */
+public class Aluno extends Pessoa{
+    
+    private int matricula;
+    private String curso;
+    
+    public void PagarMensalidade(){
+        
+    }
+    
+}
---
+++ ProjetoPessoa2/src/main/java/ProjetoPessoa2/Bolsista.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProjetoPessoa2;
+
+/**
+ *
+ * @author Geovani-PC
+ */
+public class Bolsista extends Aluno{
+    
+    private int bolsa;
+    
+    public void RenovarBolsa(){
+        
+    }
+    
+    @Override
+    public void PagarMensalidade(){
+        
+    }
+}
